770574b65582fb9d7c98f7bab23f7f7f8bf7cdc7
MogyorosiGabor <gabor.mogyi@gmail.com>
2024-07-17 20:37:17 +0200

    Update index.html

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,29 +1,30 @@
-
 if (window.innerWidth < 768) {
-	[].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
-		elem.classList.remove('animated');
-		elem.removeAttribute('data-bss-hover-animate');
-		elem.removeAttribute('data-aos');
-		elem.removeAttribute('data-bss-parallax-bg');
-		elem.removeAttribute('data-bss-scroll-zoom');
-	});
+    [].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(function (elem) {
+        elem.classList.remove('animated');
+        elem.removeAttribute('data-bss-hover-animate');
+        elem.removeAttribute('data-aos');
+        elem.removeAttribute('data-bss-parallax-bg');
+        elem.removeAttribute('data-bss-scroll-zoom');
+    });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-	if ('AOS' in window) {
-		AOS.init();
-	}
+document.addEventListener('DOMContentLoaded', function () {
+    // AOS initialization
+    ('AOS' in window) && AOS.init();
 
-	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
-	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-	  return new bootstrap.Tooltip(tooltipTriggerEl, {
-		  trigger: "hover"
-	  });
-	})
+    // Bootstrap tooltop initialization
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl, {
+            trigger: "hover",
+            customClass: "custom-tooltip"
+        });
+    });
 
-	var charts = document.querySelectorAll('[data-bss-chart]');
+    // Chart.js initialization
+    var charts = document.querySelectorAll('[data-bss-chart]');
 
-	for (var chart of charts) {
-		chart.chart = new Chart(chart, JSON.parse(chart.dataset.bssChart));
-	}
+    for (var chart of charts) {
+        chart.chart = new Chart(chart, JSON.parse(chart.dataset.bssChart));
+    }
 }, false);
