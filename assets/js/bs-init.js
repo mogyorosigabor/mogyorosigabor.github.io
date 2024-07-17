818c755b687cc5519514e1c94d2b90a9cb749f46
MogyorosiGabor <gabor.mogyi@gmail.com>
2024-07-17 21:48:53 +0200

    Update index.html

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,8 +1,8 @@
 if (window.innerWidth < 768) {
     [].slice.call(document.querySelectorAll('[data-bss-disabled-mobile]')).forEach(elem => {
         elem.classList.remove('animated');
-        elem.removeAttribute('data-bss-hover-animate');
         elem.removeAttribute('data-aos');
+        elem.removeAttribute('data-bss-hover-animate');
         elem.removeAttribute('data-bss-parallax-bg');
         elem.removeAttribute('data-bss-scroll-zoom');
     });
