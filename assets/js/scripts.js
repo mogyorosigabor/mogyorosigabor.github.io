e161527feb8fc24c919cb84691a8f131bd58b4b3
MogyorosiGabor <gabor.mogyi@gmail.com>
2025-04-21 17:23:26 +0200

    Update Brand, professional knowledges.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    ['load', 'scroll'].forEach((e) => window.addEventListener(e, function () {
+    ['load', 'scroll'].forEach(e => window.addEventListener(e, function () {
         /* scroll top button hide/show */
-        document.getElementsByClassName('scroll-top')[0].style.right = (this.scrollY > 0) ? '20px' : '-70px';
+        document.getElementsByClassName('scroll-top')[0].style.right = this.scrollY > 0 ? '20px' : '-70px';
         /* scroll indicator */
-        var winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+        let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
             height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
             scrolled = (winScroll / height) * 100;
         document.getElementById('scrollIndicatorBar').style.width = scrolled + '%';
     }, false));
     /* scroll to top */
-    document.querySelector('.scroll-top').addEventListener('click', function () {
+    document.querySelector('.scroll-top').addEventListener('click', () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
@@ -17,11 +17,13 @@ document.addEventListener('DOMContentLoaded', function () {
         return false;
     });
     /* Click nav-link collapse navbar */
-    document.querySelectorAll('.navbar .nav-item .nav-link').forEach(el => el.addEventListener('click', function () {
-        document.getElementById('navcol-5').classList.remove('show');
-    }));
-    var navbarHeight = document.querySelector('.navbar')?.offsetHeight;
-    Array.from(document.querySelectorAll('a[href*="#"]')).forEach((link) => {
+    document.querySelectorAll('.navbar .nav-item .nav-link').forEach(el => {
+        el.addEventListener('click', function () {
+            document.getElementById('navcol-5').classList.remove('show');
+        });
+    });
+    let navbarHeight = document.querySelector('.navbar')?.offsetHeight;
+    Array.from(document.querySelectorAll('a[href*="#"]')).forEach(link => {
         link.addEventListener('click', function () {
             window.scrollTo({
                 top: document.querySelector(this.hash).offsetTop - navbarHeight,
